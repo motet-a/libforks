e340446063818518f081a32598b08510affba395
Antoine Motet <antoine.motet@gmail.com>
2019-09-01 11:07:45 +0200

    Add examples/execve.c

--- examples/execve.c
+++ examples/execve.c
@@ -1,38 +1,45 @@
 #include <libforks.h>
 #include <assert.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// This example program starts a child process and exits.
-// The child process keeps running during one second.
+extern char **environ;
 
 static void child_main(libforks_ServerConn conn, int socket_fd) {
+  assert(socket_fd == -1);
+
   // not strictly required but recommended
   assert(libforks_free_conn(conn) == libforks_OK);
 
-  assert(socket_fd == -1);
-  puts("child process started");
-  sleep(1);
-  puts("child process exited");
+  char * const argv[] = {
+    "echo",
+    "hello, world!",
+    NULL
+  };
+  execve("/bin/echo", argv, environ);
+  perror("execve");
+  abort();
 }
 
 int main() {
   libforks_ServerConn conn;
   assert(libforks_start(&conn) == libforks_OK);
 
+  int exit_fd;
+
   assert(libforks_fork(
     conn,
     NULL, // pid_ptr
     NULL, // socket_fd_ptr
-    NULL, // exit_fd_ptr
+    &exit_fd, // exit_fd_ptr
     child_main
   ) == libforks_OK);
 
-  // `libforks_stop` waits until the child process exits
+  libforks_ExitEvent ee;
+  assert(read(exit_fd, &ee, sizeof ee) == sizeof ee);
+
   assert(libforks_stop(conn) == libforks_OK);
-
-  puts("main process exited");
-
   return 0;
 }
 
