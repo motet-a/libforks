7a506e3ba138268947a97f8dc659b3052033b4e8
Antoine Motet <antoine.motet@gmail.com>
2019-09-07 18:13:20 +0200

    Add tests/file_descriptor_leaks.c

---
+++ tests/file_descriptor_leaks.c
@@ -0,0 +1,52 @@
+#include "./tests.h"
+
+#include <sys/resource.h>
+
+#define LOOPS 9
+
+static void child_main(libforks_ServerConn conn, int socket_fd) {
+  (void)socket_fd;
+  (void)conn;
+
+  exit(0);
+}
+
+
+int main() {
+  struct rlimit l;
+  check(getrlimit(RLIMIT_NOFILE, &l) == 0);
+  l.rlim_cur = l.rlim_max = 9;
+  check(setrlimit(RLIMIT_NOFILE, &l) == 0);
+
+  for (size_t i = 0; i < LOOPS; i++) {
+    libforks_ServerConn conn;
+    check(libforks_start(&conn) == libforks_OK);
+    check(libforks_stop(conn) == libforks_OK);
+  }
+
+  libforks_ServerConn conn;
+  check(libforks_start(&conn) == libforks_OK);
+
+  for (size_t i = 0; i < LOOPS; i++) {
+    int exit_fd;
+
+    libforks_Result r = libforks_fork(
+      conn,
+      NULL, // pid_ptr
+      NULL, // socket_fd_ptr
+      &exit_fd, // exit_fd_ptr
+      child_main
+    );
+    check(r == libforks_OK);
+
+    libforks_ExitEvent event;
+    check(read(exit_fd, &event, sizeof event) == sizeof event);
+
+    check(close(exit_fd) == 0);
+  }
+
+  check(libforks_stop(conn) == libforks_OK);
+
+  return 0;
+}
+
