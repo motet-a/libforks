65e0a45c1393007ddddb02daa762644ccf911af0
Antoine Motet <antoine.motet@gmail.com>
2019-09-08 06:28:04 +0200

    sigkill_parent_process.c: Always wait(2) after kill(2)
    …because it doesn’t work on Linux otherwise

--- tests/sigkill_parent_process.c
+++ tests/sigkill_parent_process.c
@@ -20,14 +20,22 @@ int main() {
   pid_t child_pid = fork();
   check(child_pid != -1);
   if (child_pid == 0) {
+    check(close(pipe_fds[0]) == 0);
     parent_main();
     abort();
   }
+
+  check(close(pipe_fds[1]) == 0);
 
   pid_t serv_pid;
   check(read(pipe_fds[0], &serv_pid, sizeof serv_pid) == sizeof serv_pid);
 
   check(kill(child_pid, SIGKILL) == 0);
+
+  int wait_status;
+  check(wait(&wait_status) == child_pid);
+  check(WIFSIGNALED(wait_status));
+  check(WTERMSIG(wait_status) == SIGKILL);
 
   while (true) {
     if (kill(serv_pid, 0) == -1) {
